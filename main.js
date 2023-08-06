d6830fac1844ac99b9a676c49821f2178169329e
minyeongg <126530262+minyeongg@users.noreply.github.com>
2023-08-06 23:34:13 +0900

    영화진흥위원회 데이터 화면에 출력

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
+src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
 const API_KEY = "91fb5e4126753eefb783bf60eb1d8a40";
 async function getData() {
+  newPage;
+  const date = document.frm.movieSearch.value;
   const url =
-    "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=91fb5e4126753eefb783bf60eb1d8a40&targetDt=20120101";
-  //   const response = await fetch(url);
-  //   const data = await response.json();
-  //   console.log("data", data);
-  //   const movies = data.boxOfficeResult.dailyBoxOfficeList;
+    "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=91fb5e4126753eefb783bf60eb1d8a40&targetDt=${date}";
   $.getJSON(url, function (data) {
     let type = data.boxofficeType;
     let showRange = data.showRange;
@@ -16,4 +15,7 @@ async function getData() {
     let audiAcc = data.audiAcc;
   }); // url에서 Json 파일 로드. 콜백 함수에서 Json 파일을 이용하여 로드된 데이터를 처리
 }
-getData();
+
+function newPage() {
+  window.location.href = "./serach.html";
+}
